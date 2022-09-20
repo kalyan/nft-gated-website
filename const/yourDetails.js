c0ee7dc408b128cff6a9ab6145008e9c2afe9ccd
Kalyanjit Hatibaruah <kalyanjit@gmail.com>
2022-09-20 06:20:10 +0530

    update

--- const/yourDetails.js
+++ const/yourDetails.js
@@ -1,5 +1,5 @@
 // Replace this with your contract address
-export const contractAddress = "0x1fCbA150F05Bbe1C9D21d3ab08E35D682a4c41bF";
+export const contractAddress = "0x1fCbA150F05Bbe1C9D21d3ab08E35D682a4c41bF0x76e2F1Dd3bc15660Af400EAB62567B539442F4B0";
 
 // Replace this with your domain name.
 export const domainName = "example.com";
